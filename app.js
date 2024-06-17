c19960e5600a30259c44b6c7f4f03416029fef64
gitGastong3 <chicken333chicken@gmail.com>
2024-06-18 04:25:07 +0900

    update

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+
+// 必要なライブラリー
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// ルート用モジュールのロード
+// -> index.jsへ
 var indexRouter = require('./routes/index');
+// -> users.jsへ
 var usersRouter = require('./routes/users');
 
+// expressオブジェクトの作成
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// 関数の組み込み
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ルート用、エラー用のapp.use
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -38,4 +46,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// expressにappを適用
 module.exports = app;
